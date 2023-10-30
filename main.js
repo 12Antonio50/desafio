a6772383325bd7c885274acf066f619160ce05a7
12Antonio50 <ja.mojica.a@edu.utc.mx>
2023-10-29 19:57:44 -0600

    Regreso

--- main.js
+++ main.js
@@ -7,36 +7,3 @@ document.addEventListener('mousemove', (event) => {
     palabra.style.left = x + 'px';
     palabra.style.top = y + 'px';
 });
-const container = document.getElementById('container');
-const words = ['Te amo', '¿Me perdonas?', 'Vuelve', 'Regresa', 'Please'];
-
-function randomPosition() {
-    const randomX = Math.random() * (window.innerWidth - 100);
-    const randomY = Math.random() * (window.innerHeight - 100);
-    return { x: randomX, y: randomY };
-}
-
-function createWordElement(word) {
-    const newWord = document.createElement('div');
-    newWord.className = 'word';
-    newWord.textContent = word;
-    container.appendChild(newWord);
-    return newWord;
-}
-
-function animateWords() {
-    words.forEach(word => {
-        const wordElement = createWordElement(word);
-        const position = randomPosition();
-        wordElement.style.left = position.x + 'px';
-        wordElement.style.top = position.y + 'px';
-    });
-}
-
-animateWords();
-
-container.addEventListener('animationiteration', () => {
-    
-    container.innerHTML = '';
-    animateWords(); 
-});
